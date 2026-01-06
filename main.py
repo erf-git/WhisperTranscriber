f5108dbace2cc4f01761b863a74308f6bec1cc9a
unknown <etfeldman@optonline.net>
2026-01-06 14:32:38 -0500

    moving files to dif folder

---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+main.py
+
+Usage:
+    python main.py /[PATH_TO_AUDIO_FILE]/file.txt
+"""
+
+import whisper
+import sys
+import argparse
+from pathlib import Path
+
+FILE_DIR = None # Global variable to store working file directory
+
+def main() -> None: 
+    global FILE_DIR
+    FILE_DIR = Path.cwd() # Set working file directory
+    
+    OUT_DIR = FILE_DIR / "out"
+    OUT_DIR.mkdir(exist_ok=True)  # Create directory if it doesn't exist
+    
+    parser = argparse.ArgumentParser(
+        description="Provde audio file name to transcribe."
+    )
+    parser.add_argument("filename", help="Name of the audio file")
+
+    args = parser.parse_args()
+    filename = args.filename
+    audio_file_path = FILE_DIR / "audio" / filename
+
+    print("Selected audio file: ", audio_file_path)
+
+    if not audio_file_path.exists():
+        print(f"Error: Audio file not found: {audio_file_path}", file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        result = transcribe_audio(audio_file_path)        
+        open(OUT_DIR / "transcription.txt", "w").write(result)
+        print("Transcription complete.")
+    except Exception as exc:
+        print(f"Error: {exc}", file=sys.stderr)
+        sys.exit(2)
+
+def transcribe_audio(file_path: Path) -> str:
+    model = whisper.load_model("base")
+    result = model.transcribe(str(file_path), fp16=False)
+    return result["text"]
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-print("hi")
-print("bye")
+import whisper
+model = whisper.load_model("base")
+result = model.transcribe("Recording(3).m4a", fp16=False)
+print(result["text"])
